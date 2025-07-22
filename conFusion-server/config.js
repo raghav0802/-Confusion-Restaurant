2ad6635146d42f589ba091330d466ad2d854f653
Krishan Agarwal <2017UEE1640@mnit.ac.in>
2025-07-22 16:20:40 +0530

    Updated config.js with MongoDB URI

--- conFusion-server/config.js
+++ conFusion-server/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://admin:mnit@123@confusion.dhq67.mongodb.net/conFusion?retryWrites=true&w=majority',
+    'mongoUrl' : 'mongodb+srv://Raghav0802:<raghav_pro>@cluster0.l1mrrhj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     'facebook': {
         clientId: '278041383457931',
         clientSecret: 'd133234dba7e32fa5860c9b71cdd4183'
