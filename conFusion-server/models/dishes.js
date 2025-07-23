81ef750c9ade05f9bea7be203d857241c11a63cf
Raghav Mandhana <120410432+raghav0802@users.noreply.github.com>
2025-07-23 22:46:26 +0530

    changes

--- conFusion-server/models/dishes.js
+++ conFusion-server/models/dishes.js
@@ -1,45 +1,56 @@
-const mongoose = require('mongoose');
-require('mongoose-currency').loadType(mongoose);
+const mongoose = require("mongoose");
+require("mongoose-currency").loadType(mongoose);
 const Currency = mongoose.Types.Currency;
 
 const Schema = mongoose.Schema;
 
-
-const dishSchema = new Schema({
+const dishSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     label: {
-        type: String,
-        default: ''
+      type: String,
+      default: "",
     },
     price: {
-        type: Currency,
-        required: true,
-        min: 0
+      type: Currency,
+      required: true,
+      min: 0,
     },
     featured: {
-        type: Boolean,
-        default:false
+      type: Boolean,
+      default: false,
     },
-},{
-    timestamps: true
+  },
+  {
+    timestamps: true,
+  },
+);
+
+dishSchema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "dish",
 });
 
-var Dishes = mongoose.model('Dish', dishSchema);
+// Make sure virtual fields are included in JSON output
+dishSchema.set("toJSON", { virtuals: true });
+dishSchema.set("toObject", { virtuals: true });
 
+var Dishes = mongoose.model("Dish", dishSchema);
 module.exports = Dishes;
--- conFusion-server/routes/dishRouter.js
+++ conFusion-server/routes/dishRouter.js
@@ -1,85 +1,150 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const Dishes = require('../models/dishes');
-var authenticate = require('../authenticate');
-const cors = require('./cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Dishes = require("../models/dishes");
+var authenticate = require("../authenticate");
+const cors = require("./cors");
+const Comments = require("../models/comments");
 
 const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
-dishRouter.route('/')
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.get(cors.cors,(req,res,next) => {
+dishRouter
+  .route("/")
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
     Dishes.find(req.query)
-    .populate('comments.author')
-    .then((dishes) => {
+      .populate({
+        path: "comments",
+        populate: {
+          path: "author",
+        },
+      })
+      .then((dishes) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.json(dishes);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.post(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
-    Dishes.create(req.body)
-    .then((dish) => {
-        console.log('Dish Created ', dish);
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(dish);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.put(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-})
-.delete(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
-    Dishes.remove({})
-    .then((resp) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+      })
+      .catch((err) => {
+        console.log(new Array(10).fill("\n").join("\n"));
+        console.log(err);
+        next(err);
+      });
+  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Dishes.create(req.body)
+        .then(
+          (dish) => {
+            console.log("Dish Created ", dish);
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(dish);
+          },
+          (err) => next(err),
+        )
+        .catch((err) => next(err));
+    },
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end("PUT operation not supported on /dishes");
+    },
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Dishes.remove({})
+        .then(
+          (resp) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(resp);
+          },
+          (err) => next(err),
+        )
+        .catch((err) => next(err));
+    },
+  );
 
-dishRouter.route('/:dishId')
-.options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
-.get(cors.cors,(req,res,next) => {
+dishRouter
+  .route("/:dishId")
+  .options(cors.corsWithOptions, (req, res) => {
+    res.sendStatus(200);
+  })
+  .get(cors.cors, (req, res, next) => {
     Dishes.findById(req.params.dishId)
-    .populate('comments.author')
-    .then((dish) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(dish);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.post(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-})
-.put(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
-    Dishes.findByIdAndUpdate(req.params.dishId, {
-        $set: req.body
-    }, { new: true })
-    .then((dish) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(dish);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
-.delete(cors.corsWithOptions,authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
-    Dishes.findByIdAndRemove(req.params.dishId)
-    .then((resp) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json(resp);
-    }, (err) => next(err))
-    .catch((err) => next(err));
-});
+      .populate("comments.author")
+      .then(
+        (dish) => {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.json(dish);
+        },
+        (err) => next(err),
+      )
+      .catch((err) => next(err));
+  })
+  .post(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      res.statusCode = 403;
+      res.end("POST operation not supported on /dishes/" + req.params.dishId);
+    },
+  )
+  .put(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Dishes.findByIdAndUpdate(
+        req.params.dishId,
+        {
+          $set: req.body,
+        },
+        { new: true },
+      )
+        .then(
+          (dish) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(dish);
+          },
+          (err) => next(err),
+        )
+        .catch((err) => next(err));
+    },
+  )
+  .delete(
+    cors.corsWithOptions,
+    authenticate.verifyUser,
+    authenticate.verifyAdmin,
+    (req, res, next) => {
+      Dishes.findByIdAndRemove(req.params.dishId)
+        .then(
+          (resp) => {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.json(resp);
+          },
+          (err) => next(err),
+        )
+        .catch((err) => next(err));
+    },
+  );
 
 module.exports = dishRouter;
